5c0c6ffae56184374c554af08784a72c71037303
chirayu Baij <chirayu200@gmail.com>
2025-07-03 17:41:11 +0545

    viewmodel completed

--- lib/features/auth/presentation/view_model/auth_view_model.dart
+++ lib/features/auth/presentation/view_model/auth_view_model.dart
@@ -1,95 +1,95 @@
 
-// import 'package:e_commerce/config/constant/show_snackbar.dart';
-// import 'package:e_commerce/config/router/app_routes.dart';
-// import 'package:e_commerce/features/auth/domain/usecase/login_usecase.dart';
-// import 'package:e_commerce/features/auth/domain/usecase/register_usecase.dart';
-// import 'package:e_commerce/features/auth/presentation/state/auth_state.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_easyloading/flutter_easyloading.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:e_commerce/config/constant/show_snackbar.dart';
+import 'package:e_commerce/config/router/app_routes.dart';
+import 'package:e_commerce/features/auth/domain/usecase/login_usecase.dart';
+import 'package:e_commerce/features/auth/domain/usecase/register_usecase.dart';
+import 'package:e_commerce/features/auth/presentation/state/auth_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// final authViewModelProvider = StateNotifierProvider<AuthViewModel, AuthState>((
-//   ref,
-// ) {
-//   return AuthViewModel(ref.read(registerUsecaseProvider), ref.read(loginUsecaseProvider));
-// });
+final authViewModelProvider = StateNotifierProvider<AuthViewModel, AuthState>((
+  ref,
+) {
+  return AuthViewModel(ref.read(registerUsecaseProvider), ref.read(loginUsecaseProvider));
+});
 
-// class AuthViewModel extends StateNotifier<AuthState> {
-//   final RegisterUsecase registerUsecase;
-//   final LoginUsecase loginUsecase;
+class AuthViewModel extends StateNotifier<AuthState> {
+  final RegisterUsecase registerUsecase;
+  final LoginUsecase loginUsecase;
 
-//   AuthViewModel(this.registerUsecase, this.loginUsecase) : super(AuthState.initial());
+  AuthViewModel(this.registerUsecase, this.loginUsecase) : super(AuthState.initial());
 
-//   Future<void> register({
-//      required String fullName,
-//     required String email,
-//     required String userName,
-//     required String phoneNumber,
-//     required String password,
-//     required BuildContext context
-//   }) async {
-//     state = state.copyWith(isLoading: false);
-//     final result = await registerUsecase.register(fullName: fullName, email: email, userName: userName, phoneNumber: phoneNumber, password: password);
+  Future<void> register({
+     required String fullName,
+    required String email,
+    required String userName,
+    required String phoneNumber,
+    required String password,
+    required BuildContext context
+  }) async {
+    state = state.copyWith(isLoading: false);
+    final result = await registerUsecase.register(fullName: fullName, email: email, userName: userName, phoneNumber: phoneNumber, password: password);
 
-//     result.fold(
-//       (failure) {
-//         state = state.copyWith(error: failure.error, showMessage: true);
-//         showSnackBar(
-//           message: failure.error,
-//           context: context,
-//           color: Colors.red,
-//         );
-//       },
-//       (success) {
-//         state = state.copyWith(
-//           isLoading: false,
-//           error: null,
-//           showMessage: true,
-//         );
-//         EasyLoading.show(
-//           status: 'Please Wait...',
-//           maskType: EasyLoadingMaskType.black,
-//         );
-//         Future.delayed(const Duration(seconds: 2), () {
-//           EasyLoading.showSuccess('Register Successfully');
-//           Navigator.pushNamed(context, AppRoutes.loginRoute);
-//           EasyLoading.dismiss();
-//         });
-//       },
-//     );
-//   }
+    result.fold(
+      (failure) {
+        state = state.copyWith(error: failure.error, showMessage: true);
+        showSnackBar(
+          message: failure.error,
+          context: context,
+          color: Colors.red,
+        );
+      },
+      (success) {
+        state = state.copyWith(
+          isLoading: false,
+          error: null,
+          showMessage: true,
+        );
+        EasyLoading.show(
+          status: 'Please Wait...',
+          maskType: EasyLoadingMaskType.black,
+        );
+        Future.delayed(const Duration(seconds: 2), () {
+          EasyLoading.showSuccess('Register Successfully');
+          Navigator.pushNamed(context, AppRoutes.loginRoute);
+          EasyLoading.dismiss();
+        });
+      },
+    );
+  }
 
-//   Future<void> login(
-//      {
-//       required String userName, required String password, required BuildContext context
-//      }) async {
-//     state = state.copyWith(isLoading: false);
-//     final result = await loginUsecase.login(userName: userName, password: password);
-//     result.fold((failure) {
-//       state = state.copyWith(
-//         error: failure.error.toString(),
-//         showMessage: true,
-//       );
-//       showSnackBar(
-//           message: failure.error.toString(),
-//           context: context,
-//           color: Colors.red);
-//     }, (success) {
-//       state = state.copyWith(
-//         isLoading: false,
-//         error: null,
-//         showMessage: true,
-//       );
-//             EasyLoading.show(status: 'Please Wait...', maskType: EasyLoadingMaskType.black);
+  Future<void> login(
+     {
+      required String userName, required String password, required BuildContext context
+     }) async {
+    state = state.copyWith(isLoading: false);
+    final result = await loginUsecase.login(userName: userName, password: password);
+    result.fold((failure) {
+      state = state.copyWith(
+        error: failure.error.toString(),
+        showMessage: true,
+      );
+      showSnackBar(
+          message: failure.error.toString(),
+          context: context,
+          color: Colors.red);
+    }, (success) {
+      state = state.copyWith(
+        isLoading: false,
+        error: null,
+        showMessage: true,
+      );
+            EasyLoading.show(status: 'Please Wait...', maskType: EasyLoadingMaskType.black);
 
-//       Future.delayed(const Duration(seconds: 2), () {
-//         Navigator.pushReplacementNamed(context, AppRoutes.bootomNavRoute);
-//       EasyLoading.showSuccess('Loggedin in',);
-//         EasyLoading.dismiss();
-//       });
-//     });
-//   }
-// }
+      Future.delayed(const Duration(seconds: 2), () {
+        Navigator.pushReplacementNamed(context, AppRoutes.bootomNavRoute);
+      EasyLoading.showSuccess('Loggedin in',);
+        EasyLoading.dismiss();
+      });
+    });
+  }
+}
 
-// class AppRoute {
-// }
+class AppRoute {
+}
