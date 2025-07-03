397beed6e5b82e9fc37ef957a38911130f562aef
chirayu Baij <chirayu200@gmail.com>
2025-07-03 17:39:24 +0545

    login

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -1,168 +1,168 @@
 
-// import 'package:e_commerce/config/router/app_routes.dart';
-// import 'package:e_commerce/features/auth/presentation/view_model/auth_view_model.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:e_commerce/config/router/app_routes.dart';
+import 'package:e_commerce/features/auth/presentation/view_model/auth_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// class LoginView extends ConsumerStatefulWidget {
-//   const LoginView({super.key});
+class LoginView extends ConsumerStatefulWidget {
+  const LoginView({super.key});
 
-//   @override
-//   ConsumerState<ConsumerStatefulWidget> createState() => _LoginViewState();
-// }
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginViewState();
+}
 
-// class _LoginViewState extends ConsumerState<LoginView> {
-//   final _formKey = GlobalKey<FormState>();
+class _LoginViewState extends ConsumerState<LoginView> {
+  final _formKey = GlobalKey<FormState>();
 
-//   final userNameController = TextEditingController();
-//   final passwordController = TextEditingController();
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
 
-//   void _login() {
-//     if (_formKey.currentState!.validate()) {
-//       final userName = userNameController.text.trim();
-//       final password = passwordController.text.trim();
-//       ref
-//           .read(authViewModelProvider.notifier)
-//           .login(userName: userName, password: password, context: context);
-//     }
-//   }
+  void _login() {
+    if (_formKey.currentState!.validate()) {
+      final userName = userNameController.text.trim();
+      final password = passwordController.text.trim();
+      ref
+          .read(authViewModelProvider.notifier)
+          .login(userName: userName, password: password, context: context);
+    }
+  }
 
-//   @override
-//   void dispose() {
-//     userNameController.dispose();
-//     passwordController.dispose();
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    userNameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final state = ref.watch(authViewModelProvider);
-//     return Scaffold(
-//       backgroundColor: Colors.grey[100],
-//       body: SafeArea(
-//         child: GestureDetector(
-//           onTap: () => FocusScope.of(context).unfocus(),
-//           child: SingleChildScrollView(
-//             padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 48),
-//             child: Form(
-//               key: _formKey,
-//               child: Column(
-//                 children: [
-//                   const Text(
-//                     'Welcome Back',
-//                     style: TextStyle(
-//                       fontSize: 28,
-//                       fontWeight: FontWeight.bold,
-//                       color: Colors.deepOrange,
-//                     ),
-//                   ),
-//                   const SizedBox(height: 32),
-//                   Card(
-//                     elevation: 3,
-//                     shape: RoundedRectangleBorder(
-//                       borderRadius: BorderRadius.circular(16),
-//                     ),
-//                     shadowColor: Colors.black12,
-//                     child: Column(
-//                       children: [
-//                         _buildTextField(
-//                           controller: userNameController,
-//                           label: "Username",
-//                           icon: Icons.email_outlined,
-//                           keyboardType: TextInputType.emailAddress,
-//                           validator: (value) =>
-//                               value!.isEmpty ? 'Email is required' : null,
-//                         ),
-//                         const SizedBox(height: 20),
-//                         _buildTextField(
-//                           controller: passwordController,
-//                           label: "Password",
-//                           icon: Icons.lock_outline,
-//                           obscureText: true,
-//                           validator: (value) =>
-//                               value!.isEmpty ? 'Password is required' : null,
-//                         ),
-//                       ],
-//                     ),
-//                   ),
-//                   const SizedBox(height: 30),
-//                   SizedBox(
-//                     width: double.infinity,
-//                     height: 48,
-//                     child: ElevatedButton(
-//                       onPressed: state.isLoading ? null : _login,
-//                       style: ElevatedButton.styleFrom(
-//                         backgroundColor: Colors.deepOrange,
-//                         shape: RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.circular(12),
-//                         ),
-//                       ),
-//                       child: state.isLoading
-//                           ? const CircularProgressIndicator(color: Colors.white)
-//                           : const Text(
-//                               'Login',
-//                               style: TextStyle(
-//                                 fontSize: 16,
-//                                 fontWeight: FontWeight.w500,
-//                                 color: Colors.white,
-//                               ),
-//                             ),
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding: const EdgeInsets.all(20.0),
-//                     child: InkWell(
-//                       onTap: () =>
-//                           Navigator.pushNamed(context, AppRoutes.registerRoute),
-//                       child: Text("Don't have an account? Register"),
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
+  @override
+  Widget build(BuildContext context) {
+    final state = ref.watch(authViewModelProvider);
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 48),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  const Text(
+                    'Welcome Back',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepOrange,
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  Card(
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    shadowColor: Colors.black12,
+                    child: Column(
+                      children: [
+                        _buildTextField(
+                          controller: userNameController,
+                          label: "Username",
+                          icon: Icons.email_outlined,
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) =>
+                              value!.isEmpty ? 'Email is required' : null,
+                        ),
+                        const SizedBox(height: 20),
+                        _buildTextField(
+                          controller: passwordController,
+                          label: "Password",
+                          icon: Icons.lock_outline,
+                          obscureText: true,
+                          validator: (value) =>
+                              value!.isEmpty ? 'Password is required' : null,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: state.isLoading ? null : _login,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepOrange,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      child: state.isLoading
+                          ? const CircularProgressIndicator(color: Colors.white)
+                          : const Text(
+                              'Login',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white,
+                              ),
+                            ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: InkWell(
+                      onTap: () =>
+                          Navigator.pushNamed(context, AppRoutes.registerRoute),
+                      child: Text("Don't have an account? Register"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-//   Widget _buildTextField({
-//     required TextEditingController controller,
-//     required String label,
-//     required IconData icon,
-//     TextInputType keyboardType = TextInputType.text,
-//     bool obscureText = false,
-//     required String? Function(String?) validator,
-//   }) {
-//     return TextFormField(
-//       controller: controller,
-//       keyboardType: keyboardType,
-//       obscureText: obscureText,
-//       validator: validator,
-//       style: const TextStyle(fontSize: 15),
-//       decoration: InputDecoration(
-//         prefixIcon: Icon(icon, color: Colors.deepOrange),
-//         labelText: label,
-//         floatingLabelStyle: const TextStyle(color: Colors.deepOrange),
-//         contentPadding: const EdgeInsets.symmetric(
-//           horizontal: 16,
-//           vertical: 14,
-//         ),
-//         filled: true,
-//         fillColor: Colors.grey[50],
-//         enabledBorder: OutlineInputBorder(
-//           borderSide: BorderSide(color: Colors.grey.shade300),
-//           borderRadius: BorderRadius.circular(10),
-//         ),
-//         focusedBorder: OutlineInputBorder(
-//           borderSide: const BorderSide(color: Colors.deepOrange, width: 1.4),
-//           borderRadius: BorderRadius.circular(10),
-//         ),
-//         errorBorder: OutlineInputBorder(
-//           borderSide: BorderSide(color: Colors.red.shade300),
-//           borderRadius: BorderRadius.circular(10),
-//         ),
-//       ),
-//     );
-//   }
-// }
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    required IconData icon,
+    TextInputType keyboardType = TextInputType.text,
+    bool obscureText = false,
+    required String? Function(String?) validator,
+  }) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      validator: validator,
+      style: const TextStyle(fontSize: 15),
+      decoration: InputDecoration(
+        prefixIcon: Icon(icon, color: Colors.deepOrange),
+        labelText: label,
+        floatingLabelStyle: const TextStyle(color: Colors.deepOrange),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 14,
+        ),
+        filled: true,
+        fillColor: Colors.grey[50],
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey.shade300),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.deepOrange, width: 1.4),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red.shade300),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+}
