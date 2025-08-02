f8acac0fa6790396fbbff3d6dc5bec4a512bcfc1
chirayu Baij <chirayu200@gmail.com>
2025-08-02 13:51:37 +0545

    constant updated

--- lib/config/constant/api_endpoints.dart
+++ lib/config/constant/api_endpoints.dart
@@ -1,27 +1,27 @@
-// class ApiEndpoints {
-//   ApiEndpoints._();
+class ApiEndpoints {
+  ApiEndpoints._();
 
-//   static const Duration connectionTimeout = Duration(seconds: 3000);
-//   static const Duration recieveTimeout = Duration(seconds: 3000);
+  static const Duration connectionTimeout = Duration(seconds: 3000);
+  static const Duration recieveTimeout = Duration(seconds: 3000);
 
-//   //For Android
-//   static const String baseUrl = 'http://10.0.2.2:4000/';
-//   //For Mac
-//   // static const String baseUrl = "http://localhost:4000/";
+  //For Android
+  static const String baseUrl = 'http://10.0.2.2:4000/';
+  //For Mac
+  // static const String baseUrl = "http://localhost:4000/";
 
-//   //Auth Section
-//   static const String register = "api/user/register";
-//   static const String login = "api/user/login";
-//   static const String profile = "api/user/profile";
+  //Auth Section
+  static const String register = "api/user/register";
+  static const String login = "api/user/login";
+  static const String profile = "api/user/profile";
 
-//   //Product Section
-//   static const String allProducts = "api/product/list";
-//   static const String productByID = "api/product/single";
+  //Product Section
+  static const String allProducts = "api/product/list";
+  static const String productByID = "api/product/single";
 
-//   //Cart Section
-//   static const String addToCart = "api/cart/add";
-//   static const String allCart = "api/cart/list";
-//   static const String removeFromCart = "api/cart/remove";
+  //Cart Section
+  static const String addToCart = "api/cart/add";
+  static const String allCart = "api/cart/list";
+  static const String removeFromCart = "api/cart/remove";
 
 
-// }
+}
