6d9940d6813c0820f84ea4fee79c4fb99b96b7e9
chirayu Baij <chirayu200@gmail.com>
2025-08-02 13:54:59 +0545

    app routes

--- lib/config/router/app_routes.dart
+++ lib/config/router/app_routes.dart
@@ -1,20 +1,20 @@
 
-import 'package:e_commerce/features/auth/presentation/view/login_view.dart';
-import 'package:e_commerce/features/auth/presentation/view/register_view.dart';
-import 'package:e_commerce/features/home/presentation/view/bottom_view.dart';
+// import 'package:e_commerce/features/auth/presentation/view/login_view.dart';
+// import 'package:e_commerce/features/auth/presentation/view/register_view.dart';
+// import 'package:e_commerce/features/home/presentation/view/bottom_view.dart';
 
-class AppRoutes {
-  AppRoutes._();
+// class AppRoutes {
+//   AppRoutes._();
 
-  static const String registerRoute = "/register";
-  static const String loginRoute = "/login";
-  static const String bootomNavRoute = "/bootomNav";
+//   static const String registerRoute = "/register";
+//   static const String loginRoute = "/login";
+//   static const String bootomNavRoute = "/bootomNav";
 
-  static getApplicationRoute() {
-    return {
-      registerRoute: (context) => RegisterView(),
-      loginRoute: (context) => LoginView(),
-      bootomNavRoute: (context) => BottomView(),
-    };
-  }
-}
+//   static getApplicationRoute() {
+//     return {
+//       registerRoute: (context) => RegisterView(),
+//       loginRoute: (context) => LoginView(),
+//       bootomNavRoute: (context) => BottomView(),
+//     };
+//   }
+// }
