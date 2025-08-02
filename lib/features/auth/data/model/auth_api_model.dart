08b7fea22d11336e3fa0828c535d30367b36b0e0
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:02:52 +0545

    Auth model updated

--- lib/features/auth/data/model/auth_api_model.dart
+++ lib/features/auth/data/model/auth_api_model.dart
@@ -1,30 +1,28 @@
-// import 'package:json_annotation/json_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'auth_api_model.g.dart';
 
-// part 'auth_api_model.g.dart';
+@JsonSerializable()
+class AuthApiModel {
+  AuthApiModel(
+      {this.id,
+      this.name,
+      this.email,
+      this.password,
+      this.isAdmin,
+      this.cartData,
+      this.v});
+  @JsonKey(name: "_id")
+  final String? id;
+  final String? name;
+  final String? email;
+  final String? password;
+  @JsonKey(defaultValue: false)
+  final bool? isAdmin;
+  final Map<String, int>? cartData;
+  final int? v;
+  Map<String, dynamic> toJson() => _$AuthApiModelToJson(this);
 
-// @JsonSerializable()
-// class AuthApiModel {
-//   AuthApiModel({
-//     this.id,
-//     this.name,
-//     this.email,
-//     this.password,
-//     this.isAdmin,
-//     this.cartData,
-//     this.v
-//   });
-//   @JsonKey(name: "_id")
-//    final String? id;
-//     final String? name;
-//     final String? email;
-//     final String? password;
-//     @JsonKey(defaultValue: false)
-//     final bool? isAdmin;
-//     final Map<String, int>? cartData;
-//     final int? v;
-//   Map<String, dynamic> toJson() => _$AuthApiModelToJson(this);
-
-//   factory AuthApiModel.fromJson(Map<String, dynamic> json) => _$AuthApiModelFromJson(json);
-// }
-
+  factory AuthApiModel.fromJson(Map<String, dynamic> json) =>
+      _$AuthApiModelFromJson(json);
+}
