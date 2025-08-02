dcf19fa37ff59a6debc0d7d5a63f91bdb3c1e203
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:04:01 +0545

    repostiory auth updated

--- lib/features/auth/data/repository/auth_remote_repository.dart
+++ lib/features/auth/data/repository/auth_remote_repository.dart
@@ -1,25 +1,27 @@
-// import 'package:dartz/dartz.dart';
-// import 'package:e_commerce/core/failure/failure.dart';
-// import 'package:e_commerce/features/auth/data/datasource/auth_remote_datasource.dart';
-// import 'package:e_commerce/features/auth/domain/repository/i_auth_repository.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-// final authRemoteRepositoryProvider = Provider<AuthRemoteRepository>(
-//     (ref) => AuthRemoteRepository(ref.watch(authRemoteDataSourceProvider)));
+import 'package:dartz/dartz.dart';
+import 'package:e_commerce/core/failure/failure.dart';
+import 'package:e_commerce/features/auth/data/datasource/auth_remote_datasource.dart';
+import 'package:e_commerce/features/auth/domain/repository/i_auth_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// class AuthRemoteRepository implements IauthRepository {
-//   final AuthRemoteDatasource authRemoteDatasource;
-//   AuthRemoteRepository(this.authRemoteDatasource);
-//   @override
-//   Future<Either<Failure, bool>> register({
-//      required String name,
-//     required String email,
-//     required String password,
-//   }) async {
-//     return authRemoteDatasource.register(name: name, email: email,  password: password);
-//   }
-  
-//   @override
-//   Future<Either<Failure, bool>> login(String userName, String password) async{
-//     return authRemoteDatasource.login(userName: userName, password: password);
-//   }
-// }
+final authRemoteRepositoryProvider = Provider<AuthRemoteRepository>(
+    (ref) => AuthRemoteRepository(ref.watch(authRemoteDataSourceProvider)));
+
+class AuthRemoteRepository implements IauthRepository {
+  final AuthRemoteDatasource authRemoteDatasource;
+  AuthRemoteRepository(this.authRemoteDatasource);
+  @override
+  Future<Either<Failure, bool>> register({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    return authRemoteDatasource.register(
+        name: name, email: email, password: password);
+  }
+
+  @override
+  Future<Either<Failure, bool>> login(String userName, String password) async {
+    return authRemoteDatasource.login(userName: userName, password: password);
+  }
+}
