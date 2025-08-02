927faab8f6a442d654da9830c755df6ffbab8cba
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:38:06 +0545

    product data source updated

--- lib/features/products/data/datasource/product_remote_datasource.dart
+++ lib/features/products/data/datasource/product_remote_datasource.dart
@@ -1,56 +1,55 @@
-// // product_remote_datasource.dart
-// import 'package:dartz/dartz.dart';
-// import 'package:dio/dio.dart';
-// import 'package:e_commerce/config/constant/api_endpoints.dart';
-// import 'package:e_commerce/core/failure/failure.dart';
-// import 'package:e_commerce/core/network/http_service.dart';
-// import 'package:e_commerce/features/products/data/model/product_api_model.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// product_remote_datasource.dart
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:e_commerce/config/constant/api_endpoints.dart';
+import 'package:e_commerce/core/failure/failure.dart';
+import 'package:e_commerce/core/network/http_service.dart';
+import 'package:e_commerce/features/products/data/model/product_api_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// final productRemoteDataSourceProvider = Provider<ProductRemoteDataSource>((
-//   ref,
-// ) {
-//   final dio = ref.read(httpServiceProvider);
-//   return ProductRemoteDataSource(dio);
-// });
+final productRemoteDataSourceProvider = Provider<ProductRemoteDataSource>((
+  ref,
+) {
+  final dio = ref.read(httpServiceProvider);
+  return ProductRemoteDataSource(dio);
+});
 
-// class ProductRemoteDataSource {
-//   final Dio dio;
-//   ProductRemoteDataSource(this.dio);
+class ProductRemoteDataSource {
+  final Dio dio;
+  ProductRemoteDataSource(this.dio);
 
-//   Future<Either<Failure, List<ProductApiModel>>> getAllProducts() async {
-//     try {
-//       final response = await dio.get(ApiEndpoints.allProducts);
-//       if (response.statusCode == 200) {
-//         final data = response.data;
-//         final productsJson = data['products'] as List;
-//         final products = productsJson
-//             .map((json) => ProductApiModel.fromJson(json))
-//             .toList();
-//         return Right(products);
-//       } else {
-//         return Left(Failure(error: 'Failed to fetch products'));
-//       }
-//     } catch (e) {
-//       return Left(Failure(error: e.toString()));
-//     }
-//   }
+  Future<Either<Failure, List<ProductApiModel>>> getAllProducts() async {
+    try {
+      final response = await dio.get(ApiEndpoints.allProducts);
+      if (response.statusCode == 200) {
+        final data = response.data;
+        final productsJson = data['products'] as List;
+        final products =
+            productsJson.map((json) => ProductApiModel.fromJson(json)).toList();
+        return Right(products);
+      } else {
+        return Left(Failure(error: 'Failed to fetch products'));
+      }
+    } catch (e) {
+      return Left(Failure(error: e.toString()));
+    }
+  }
 
-//   Future<Either<Failure, ProductApiModel>> getProductById({
-//     required String id,
-//   }) async {
-//     try {
-//       final url = "${ApiEndpoints.productByID}/$id";
-//       final response = await dio.get(url);
-//       if (response.statusCode == 200) {
-//         final data = response.data['product'];
-//          final product = ProductApiModel.fromJson(data);
-//         return Right(product);
-//       } else {
-//         return Left(Failure(error: 'Failed to fetch products'));
-//       }
-//     } catch (e) {
-//       return Left(Failure(error: e.toString()));
-//     }
-//   }
-// }
+  Future<Either<Failure, ProductApiModel>> getProductById({
+    required String id,
+  }) async {
+    try {
+      final url = "${ApiEndpoints.productByID}/$id";
+      final response = await dio.get(url);
+      if (response.statusCode == 200) {
+        final data = response.data['product'];
+        final product = ProductApiModel.fromJson(data);
+        return Right(product);
+      } else {
+        return Left(Failure(error: 'Failed to fetch products'));
+      }
+    } catch (e) {
+      return Left(Failure(error: e.toString()));
+    }
+  }
+}
