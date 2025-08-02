dc56e7a713205f20f3e76c4fa19c03e20ec19c09
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:10:59 +0545

    auth entity

--- lib/features/auth/domain/entity/auth_entity.dart
+++ lib/features/auth/domain/entity/auth_entity.dart
@@ -1,20 +1,19 @@
-// // ignore_for_file: public_member_api_docs, sort_constructors_first
-// class AuthEntity {
-
-//    final String? id;
-//     final String? name;
-//     final String? email;
-//     final String? password;
-//     final bool? isAdmin;
-//     final Map<String, int>? cartData;
-//     final int? v;
-//   AuthEntity({
-//     this.id,
-//     this.name,
-//     this.email,
-//     this.password,
-//     this.isAdmin,
-//     this.cartData,
-//     this.v,
-//   });
-// }
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class AuthEntity {
+  final String? id;
+  final String? name;
+  final String? email;
+  final String? password;
+  final bool? isAdmin;
+  final Map<String, int>? cartData;
+  final int? v;
+  AuthEntity({
+    this.id,
+    this.name,
+    this.email,
+    this.password,
+    this.isAdmin,
+    this.cartData,
+    this.v,
+  });
+}
