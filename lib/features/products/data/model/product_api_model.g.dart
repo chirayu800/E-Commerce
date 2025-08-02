eba5102f0801505df4353f483a3dbd0a5b3e0dc3
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:40:06 +0545

    product api model updated

--- lib/features/products/data/model/product_api_model.g.dart
+++ lib/features/products/data/model/product_api_model.g.dart
@@ -1,38 +1,41 @@
-// // GENERATED CODE - DO NOT MODIFY BY HAND
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
-// part of 'product_api_model.dart';
+part of 'product_api_model.dart';
 
-// // **************************************************************************
-// // JsonSerializableGenerator
-// // **************************************************************************
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
 
-// ProductApiModel _$ProductApiModelFromJson(
-//   Map<String, dynamic> json,
-// ) => ProductApiModel(
-//   id: json['_id'] as String?,
-//   name: json['name'] as String?,
-//   description: json['description'] as String?,
-//   price: (json['price'] as num?)?.toInt(),
-//   image: (json['image'] as List<dynamic>?)?.map((e) => e as String).toList(),
-//   category: json['category'] as String?,
-//   subCategory: json['subCategory'] as String?,
-//   sizes: (json['sizes'] as List<dynamic>?)?.map((e) => e as String).toList(),
-//   bestSeller: json['bestSeller'] as bool?,
-//   date: (json['date'] as num?)?.toInt(),
-//   v: (json['v'] as num?)?.toInt(),
-// );
+ProductApiModel _$ProductApiModelFromJson(
+  Map<String, dynamic> json,
+) =>
+    ProductApiModel(
+      id: json['_id'] as String?,
+      name: json['name'] as String?,
+      description: json['description'] as String?,
+      price: (json['price'] as num?)?.toInt(),
+      image:
+          (json['image'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      category: json['category'] as String?,
+      subCategory: json['subCategory'] as String?,
+      sizes:
+          (json['sizes'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      bestSeller: json['bestSeller'] as bool?,
+      date: (json['date'] as num?)?.toInt(),
+      v: (json['v'] as num?)?.toInt(),
+    );
 
-// Map<String, dynamic> _$ProductApiModelToJson(ProductApiModel instance) =>
-//     <String, dynamic>{
-//       '_id': instance.id,
-//       'name': instance.name,
-//       'description': instance.description,
-//       'price': instance.price,
-//       'image': instance.image,
-//       'category': instance.category,
-//       'subCategory': instance.subCategory,
-//       'sizes': instance.sizes,
-//       'bestSeller': instance.bestSeller,
-//       'date': instance.date,
-//       'v': instance.v,
-//     };
+Map<String, dynamic> _$ProductApiModelToJson(ProductApiModel instance) =>
+    <String, dynamic>{
+      '_id': instance.id,
+      'name': instance.name,
+      'description': instance.description,
+      'price': instance.price,
+      'image': instance.image,
+      'category': instance.category,
+      'subCategory': instance.subCategory,
+      'sizes': instance.sizes,
+      'bestSeller': instance.bestSeller,
+      'date': instance.date,
+      'v': instance.v,
+    };
