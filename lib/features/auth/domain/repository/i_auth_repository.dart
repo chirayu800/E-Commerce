f5b9b63bf6d0047a04b7afaf9f182fbecd93432a
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:11:38 +0545

    usecase auth login

--- lib/features/auth/domain/repository/i_auth_repository.dart
+++ lib/features/auth/domain/repository/i_auth_repository.dart
@@ -1,17 +1,17 @@
-import 'package:dartz/dartz.dart';
-import 'package:e_commerce/core/failure/failure.dart';
-import 'package:e_commerce/features/auth/data/repository/auth_remote_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:e_commerce/core/failure/failure.dart';
+// import 'package:e_commerce/features/auth/data/repository/auth_remote_repository.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-final iauthRepositoryProvider =
-    Provider((ref) => ref.read(authRemoteRepositoryProvider));
+// final iauthRepositoryProvider =
+//     Provider((ref) => ref.read(authRemoteRepositoryProvider));
 
-abstract class IauthRepository {
-  Future<Either<Failure, bool>> register({
-     required String name,
-    required String email,
-    required String password,
-  });
-  Future<Either<Failure, bool>> login(String userName, String password);
-}
+// abstract class IauthRepository {
+//   Future<Either<Failure, bool>> register({
+//      required String name,
+//     required String email,
+//     required String password,
+//   });
+//   Future<Either<Failure, bool>> login(String userName, String password);
+// }
