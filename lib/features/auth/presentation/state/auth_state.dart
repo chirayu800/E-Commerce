51099ae7653cdfc33bcc186f16252727b88796c2
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:13:29 +0545

    auth view

--- lib/features/auth/presentation/state/auth_state.dart
+++ lib/features/auth/presentation/state/auth_state.dart
@@ -1,48 +1,48 @@
 
-import 'package:e_commerce/features/auth/data/model/auth_api_model.dart';
+// import 'package:e_commerce/features/auth/data/model/auth_api_model.dart';
 
-class AuthState {
-  final bool isLoading;
-  final String? error;
-  final String? imageName;
-  final bool? showMessage;
-  final AuthApiModel? profileData;
+// class AuthState {
+//   final bool isLoading;
+//   final String? error;
+//   final String? imageName;
+//   final bool? showMessage;
+//   final AuthApiModel? profileData;
 
-  AuthState({
-    required this.isLoading,
-    this.error,
-    this.imageName,
-    this.showMessage,
-    this.profileData,
-  });
+//   AuthState({
+//     required this.isLoading,
+//     this.error,
+//     this.imageName,
+//     this.showMessage,
+//     this.profileData,
+//   });
 
-  factory AuthState.initial() {
-    return AuthState(
-      isLoading: false,
-      error: null,
-      imageName: null,
-      showMessage: false,
-      profileData: null,
-    );
-  }
+//   factory AuthState.initial() {
+//     return AuthState(
+//       isLoading: false,
+//       error: null,
+//       imageName: null,
+//       showMessage: false,
+//       profileData: null,
+//     );
+//   }
 
-  AuthState copyWith({
-    bool? isLoading,
-    String? error,
-    String? imageName,
-    bool? showMessage,
-    AuthApiModel? profileData,
-  }) {
-    return AuthState(
-      isLoading: isLoading ?? this.isLoading,
-      error: error ?? this.error,
-      imageName: imageName ?? this.imageName,
-      showMessage: showMessage ?? this.showMessage,
-      profileData: profileData ?? this.profileData,
-    );
-  }
+//   AuthState copyWith({
+//     bool? isLoading,
+//     String? error,
+//     String? imageName,
+//     bool? showMessage,
+//     AuthApiModel? profileData,
+//   }) {
+//     return AuthState(
+//       isLoading: isLoading ?? this.isLoading,
+//       error: error ?? this.error,
+//       imageName: imageName ?? this.imageName,
+//       showMessage: showMessage ?? this.showMessage,
+//       profileData: profileData ?? this.profileData,
+//     );
+//   }
 
-  @override
-  String toString() =>
-      'AuthState(isLoading: $isLoading, error: $error, currentUser: $profileData)';
-}
+//   @override
+//   String toString() =>
+//       'AuthState(isLoading: $isLoading, error: $error, currentUser: $profileData)';
+// }
