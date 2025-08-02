6635f2a4606ab748aed5458956d07af4d60fcb92
chirayu Baij <chirayu200@gmail.com>
2025-08-02 13:56:43 +0545

    faliure

--- lib/core/failure/failure.dart
+++ lib/core/failure/failure.dart
@@ -1,6 +1,6 @@
-class Failure {
-  final String error;
-  final String? statusCode;
+// class Failure {
+//   final String error;
+//   final String? statusCode;
 
-  Failure({required this.error, this.statusCode});
-}
+//   Failure({required this.error, this.statusCode});
+// }
