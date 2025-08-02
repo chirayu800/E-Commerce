d4f7911aa12f293403e379f4fde819aad127902c
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:00:09 +0545

    features

--- lib/core/network/http_service.dart
+++ lib/core/network/http_service.dart
@@ -1,28 +1,28 @@
-import 'package:dio/dio.dart';
-import 'package:e_commerce/config/constant/api_endpoints.dart';
-import 'package:e_commerce/core/network/dio_error_interceptor.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+// import 'package:dio/dio.dart';
+// import 'package:e_commerce/config/constant/api_endpoints.dart';
+// import 'package:e_commerce/core/network/dio_error_interceptor.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
-final httpServiceProvider =
-    Provider.autoDispose((ref) => HttpService(Dio())._dio);
+// final httpServiceProvider =
+//     Provider.autoDispose((ref) => HttpService(Dio())._dio);
 
-class HttpService {
-  final Dio _dio;
+// class HttpService {
+//   final Dio _dio;
 
-  Dio get dio => _dio;
+//   Dio get dio => _dio;
 
-  HttpService(this._dio) {
-    _dio
-      ..options.baseUrl = ApiEndpoints.baseUrl
-      ..options.connectTimeout = ApiEndpoints.connectionTimeout
-      ..options.receiveTimeout = ApiEndpoints.recieveTimeout
-      ..interceptors.add(DioErrorInterceptor())
-      ..interceptors.add(PrettyDioLogger(
-          requestHeader: true, requestBody: true, responseHeader: true))
-      ..options.headers = {
-        'Accept': 'application/json',
-        'Content-type': 'application/json',
-      };
-  }
-}
+//   HttpService(this._dio) {
+//     _dio
+//       ..options.baseUrl = ApiEndpoints.baseUrl
+//       ..options.connectTimeout = ApiEndpoints.connectionTimeout
+//       ..options.receiveTimeout = ApiEndpoints.recieveTimeout
+//       ..interceptors.add(DioErrorInterceptor())
+//       ..interceptors.add(PrettyDioLogger(
+//           requestHeader: true, requestBody: true, responseHeader: true))
+//       ..options.headers = {
+//         'Accept': 'application/json',
+//         'Content-type': 'application/json',
+//       };
+//   }
+// }
