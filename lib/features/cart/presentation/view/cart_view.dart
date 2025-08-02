991a9308cfc7a11b552cc7cf4f2a4348dbe6d020
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:24:58 +0545

    dependency added

--- lib/features/cart/presentation/view/cart_view.dart
+++ lib/features/cart/presentation/view/cart_view.dart
@@ -1,152 +1,152 @@
-import 'package:e_commerce/features/cart/presentation/view_model/cart_view_model.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:e_commerce/features/cart/presentation/view_model/cart_view_model.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CartView extends ConsumerStatefulWidget {
-  const CartView({super.key});
+// class CartView extends ConsumerStatefulWidget {
+//   const CartView({super.key});
 
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _CartViewState();
-}
+//   @override
+//   ConsumerState<ConsumerStatefulWidget> createState() => _CartViewState();
+// }
 
-class _CartViewState extends ConsumerState<CartView> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await ref.read(cartViewModelProvider.notifier).fetchUserCart();
-    });
-  }
+// class _CartViewState extends ConsumerState<CartView> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     WidgetsBinding.instance.addPostFrameCallback((_) async {
+//       await ref.read(cartViewModelProvider.notifier).fetchUserCart();
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    final cartState = ref.watch(cartViewModelProvider);
-    final cartItems = cartState.cartApiModel ?? [];
+//   @override
+//   Widget build(BuildContext context) {
+//     final cartState = ref.watch(cartViewModelProvider);
+//     final cartItems = cartState.cartApiModel ?? [];
 
-    double totalPrice = 0;
-    for (var item in cartItems) {
-      final quantity = item.quantity ?? 1;
-      final price = item.product?.price ?? 0;
-      totalPrice += price * quantity;
-    }
+//     double totalPrice = 0;
+//     for (var item in cartItems) {
+//       final quantity = item.quantity ?? 1;
+//       final price = item.product?.price ?? 0;
+//       totalPrice += price * quantity;
+//     }
 
-    return Scaffold(
-      appBar: AppBar(title: const Text("Your Cart"), centerTitle: true),
-      body: cartState.isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : cartItems.isEmpty
-              ? const Center(child: Text("No items in cart"))
-              : Column(
-                  children: [
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: cartItems.length,
-                        itemBuilder: (context, index) {
-                          final cartItem = cartItems[index];
-                          final product = cartItem.product;
-                          final imageUrl = product?.image?.first ?? '';
-                          final quantity = cartItem.quantity ?? 1;
-                          final price = product?.price ?? 0;
+//     return Scaffold(
+//       appBar: AppBar(title: const Text("Your Cart"), centerTitle: true),
+//       body: cartState.isLoading
+//           ? const Center(child: CircularProgressIndicator())
+//           : cartItems.isEmpty
+//               ? const Center(child: Text("No items in cart"))
+//               : Column(
+//                   children: [
+//                     Expanded(
+//                       child: ListView.builder(
+//                         itemCount: cartItems.length,
+//                         itemBuilder: (context, index) {
+//                           final cartItem = cartItems[index];
+//                           final product = cartItem.product;
+//                           final imageUrl = product?.image?.first ?? '';
+//                           final quantity = cartItem.quantity ?? 1;
+//                           final price = product?.price ?? 0;
 
-                          return Card(
-                            margin: const EdgeInsets.symmetric(
-                              horizontal: 12,
-                              vertical: 8,
-                            ),
-                            elevation: 3,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: ListTile(
-                              contentPadding: const EdgeInsets.all(10),
-                              leading: ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.network(
-                                  imageUrl,
-                                  width: 60,
-                                  height: 60,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              title: Text(
-                                product?.name ?? '',
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
-                              subtitle: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const SizedBox(height: 4),
-                                  Text("Quantity: $quantity"),
-                                  Text("Price: Rs. $price"),
-                                ],
-                              ),
-                              trailing: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  IconButton(
-                                    icon: const Icon(
-                                      Icons.delete,
-                                      color: Colors.red,
-                                    ),
-                                    onPressed: () async {
-                                      final userId = await ref
-                                          .read(cartViewModelProvider.notifier)
-                                          .getUserIdFromStorage();
-                                      if (userId != null &&
-                                          product?.id != null) {
-                                        await ref
-                                            .read(
-                                                cartViewModelProvider.notifier)
-                                            .removeCartItem(
-                                              userId: userId,
-                                              productId: product!.id!,
-                                              context: context,
-                                            );
-                                      }
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
-                        vertical: 12,
-                      ),
-                      decoration: const BoxDecoration(
-                        border: Border(top: BorderSide(color: Colors.grey)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            "Total",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            "Rs. ${totalPrice.toStringAsFixed(2)}",
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blueAccent,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-    );
-  }
-}
+//                           return Card(
+//                             margin: const EdgeInsets.symmetric(
+//                               horizontal: 12,
+//                               vertical: 8,
+//                             ),
+//                             elevation: 3,
+//                             shape: RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(15),
+//                             ),
+//                             child: ListTile(
+//                               contentPadding: const EdgeInsets.all(10),
+//                               leading: ClipRRect(
+//                                 borderRadius: BorderRadius.circular(10),
+//                                 child: Image.network(
+//                                   imageUrl,
+//                                   width: 60,
+//                                   height: 60,
+//                                   fit: BoxFit.cover,
+//                                 ),
+//                               ),
+//                               title: Text(
+//                                 product?.name ?? '',
+//                                 style: const TextStyle(
+//                                   fontWeight: FontWeight.bold,
+//                                   fontSize: 16,
+//                                 ),
+//                               ),
+//                               subtitle: Column(
+//                                 crossAxisAlignment: CrossAxisAlignment.start,
+//                                 children: [
+//                                   const SizedBox(height: 4),
+//                                   Text("Quantity: $quantity"),
+//                                   Text("Price: Rs. $price"),
+//                                 ],
+//                               ),
+//                               trailing: Column(
+//                                 mainAxisAlignment:
+//                                     MainAxisAlignment.spaceBetween,
+//                                 children: [
+//                                   IconButton(
+//                                     icon: const Icon(
+//                                       Icons.delete,
+//                                       color: Colors.red,
+//                                     ),
+//                                     onPressed: () async {
+//                                       final userId = await ref
+//                                           .read(cartViewModelProvider.notifier)
+//                                           .getUserIdFromStorage();
+//                                       if (userId != null &&
+//                                           product?.id != null) {
+//                                         await ref
+//                                             .read(
+//                                                 cartViewModelProvider.notifier)
+//                                             .removeCartItem(
+//                                               userId: userId,
+//                                               productId: product!.id!,
+//                                               context: context,
+//                                             );
+//                                       }
+//                                     },
+//                                   ),
+//                                 ],
+//                               ),
+//                             ),
+//                           );
+//                         },
+//                       ),
+//                     ),
+//                     Container(
+//                       padding: const EdgeInsets.symmetric(
+//                         horizontal: 16,
+//                         vertical: 12,
+//                       ),
+//                       decoration: const BoxDecoration(
+//                         border: Border(top: BorderSide(color: Colors.grey)),
+//                       ),
+//                       child: Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                         children: [
+//                           const Text(
+//                             "Total",
+//                             style: TextStyle(
+//                               fontSize: 18,
+//                               fontWeight: FontWeight.bold,
+//                             ),
+//                           ),
+//                           Text(
+//                             "Rs. ${totalPrice.toStringAsFixed(2)}",
+//                             style: const TextStyle(
+//                               fontSize: 18,
+//                               fontWeight: FontWeight.bold,
+//                               color: Colors.blueAccent,
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//     );
+//   }
+// }
