eb8824d179e3f2099b8f640da573f2f179846b71
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:41:34 +0545

    product state

--- lib/features/products/presentation/state/product_state.dart
+++ lib/features/products/presentation/state/product_state.dart
@@ -1,37 +1,37 @@
 
-import 'package:e_commerce/features/products/data/model/product_api_model.dart';
+// import 'package:e_commerce/features/products/data/model/product_api_model.dart';
 
-class ProductState {
-  final List<ProductApiModel> products;
-  final ProductApiModel? singleProduct;
-  final bool isLoading;
-  final String? error;
+// class ProductState {
+//   final List<ProductApiModel> products;
+//   final ProductApiModel? singleProduct;
+//   final bool isLoading;
+//   final String? error;
 
-  ProductState({
-    required this.products,
-     this.singleProduct,
-    required this.isLoading,
-    this.error,
-  });
+//   ProductState({
+//     required this.products,
+//      this.singleProduct,
+//     required this.isLoading,
+//     this.error,
+//   });
 
-  ProductState copyWith({
-    List<ProductApiModel>? products,
-    ProductApiModel? singleProduct,
-    bool? isLoading,
-    String? error,
-  }) {
-    return ProductState(
-      products: products ?? this.products,
-      singleProduct: singleProduct ?? this.singleProduct,
-      isLoading: isLoading ?? this.isLoading,
-      error: error,
-    );
-  }
+//   ProductState copyWith({
+//     List<ProductApiModel>? products,
+//     ProductApiModel? singleProduct,
+//     bool? isLoading,
+//     String? error,
+//   }) {
+//     return ProductState(
+//       products: products ?? this.products,
+//       singleProduct: singleProduct ?? this.singleProduct,
+//       isLoading: isLoading ?? this.isLoading,
+//       error: error,
+//     );
+//   }
 
-  factory ProductState.initial() => ProductState(
-        products: [],
-        singleProduct: null,
-        isLoading: false,
-        error: null,
-      );
-}
+//   factory ProductState.initial() => ProductState(
+//         products: [],
+//         singleProduct: null,
+//         isLoading: false,
+//         error: null,
+//       );
+// }
