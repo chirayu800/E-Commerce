efce75ffe562d2eabdfb3a449b969f6ae1d10435
chirayu Baij <chirayu200@gmail.com>
2025-08-02 14:18:39 +0545

    cart model

--- lib/features/cart/data/model/cart_api_model.dart
+++ lib/features/cart/data/model/cart_api_model.dart
@@ -1,21 +1,21 @@
-// // cart_api_model.dart
-// import 'package:e_commerce/features/products/data/model/product_api_model.dart';
-// import 'package:json_annotation/json_annotation.dart';
+// cart_api_model.dart
+import 'package:e_commerce/features/products/data/model/product_api_model.dart';
+import 'package:json_annotation/json_annotation.dart';
 
-// part 'cart_api_model.g.dart';
+part 'cart_api_model.g.dart';
 
-// @JsonSerializable()
-// class CartApiModel {
-//   CartApiModel({
-//     this.product,
-//     this.quantity,
-//   });
+@JsonSerializable()
+class CartApiModel {
+  CartApiModel({
+    this.product,
+    this.quantity,
+  });
 
-//   final ProductApiModel? product;
-//   final int? quantity;
+  final ProductApiModel? product;
+  final int? quantity;
 
-//   factory CartApiModel.fromJson(Map<String, dynamic> json) =>
-//       _$CartApiModelFromJson(json);
+  factory CartApiModel.fromJson(Map<String, dynamic> json) =>
+      _$CartApiModelFromJson(json);
 
-//   Map<String, dynamic> toJson() => _$CartApiModelToJson(this);
-// }
+  Map<String, dynamic> toJson() => _$CartApiModelToJson(this);
+}
--- lib/features/cart/data/model/cart_api_model.g.dart
+++ lib/features/cart/data/model/cart_api_model.g.dart
@@ -1,20 +1,20 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
+// // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'cart_api_model.dart';
+// part of 'cart_api_model.dart';
 
-// **************************************************************************
-// JsonSerializableGenerator
-// **************************************************************************
+// // **************************************************************************
+// // JsonSerializableGenerator
+// // **************************************************************************
 
-CartApiModel _$CartApiModelFromJson(Map<String, dynamic> json) => CartApiModel(
-  product: json['product'] == null
-      ? null
-      : ProductApiModel.fromJson(json['product'] as Map<String, dynamic>),
-  quantity: (json['quantity'] as num?)?.toInt(),
-);
+// CartApiModel _$CartApiModelFromJson(Map<String, dynamic> json) => CartApiModel(
+//   product: json['product'] == null
+//       ? null
+//       : ProductApiModel.fromJson(json['product'] as Map<String, dynamic>),
+//   quantity: (json['quantity'] as num?)?.toInt(),
+// );
 
-Map<String, dynamic> _$CartApiModelToJson(CartApiModel instance) =>
-    <String, dynamic>{
-      'product': instance.product,
-      'quantity': instance.quantity,
-    };
+// Map<String, dynamic> _$CartApiModelToJson(CartApiModel instance) =>
+//     <String, dynamic>{
+//       'product': instance.product,
+//       'quantity': instance.quantity,
+//     };
